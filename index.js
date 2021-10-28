2b14c2e283e2484221263f1c2cd47f1537a72895
Bryen Daco <72845402+BryenDaco@users.noreply.github.com>
2021-10-28 11:50:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const axios = require("axios");
+const discord = require('discord.js'); // 13.0.0, 13.0.1, 13.1.0, 13.2.0
+
+const client = new discord.Client({intents: [discord.Intents.FLAGS.DIRECT_MESSAGES, discord.Intents.FLAGS.GUILD_MESSAGES]});
 
 client.on('messageCreate', async message => {
 	const catcscans = await axios.get("https://bryendaco.github.io/catchscams.json");
@@ -7,3 +10,5 @@ client.on('messageCreate', async message => {
 		if (!!message.content && message.content.includes(scamdomains)) return console.log("Scam Link Detected!")
 	}
 });
+
+client.login("Your-Bot-Token")
